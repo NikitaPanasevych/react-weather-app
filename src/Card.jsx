06865f53237074bdf357ddae46ec77ac62a85717
DeStRaaM <20destraam03@gmail.com>
2022-07-26 18:01:50 +0200

    Minor change of style

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,10 +5,10 @@ const Card = (props) => {
 
     return(
         <div className="w-60 h-60 text-center pt-5 text-2xl ml-10 mb-10 rounded-3xl border-4 bg-[#A2B5BB] justify-center">
-            <h1>{props.city}</h1>
-            <h2>{props.temp}°C</h2>
-            <img className="h-28 w-28 m-auto" src={props.img} />
-            <h2>{props.descr}</h2>
+            <h1 className="">{props.city}</h1>
+            <h2 className="relative font-semibold">{props.temp}°C</h2>
+            <img className="h-28 w-28 m-auto relative bottom-4" src={props.img} />
+            <h2 className="relative w-3/4 left-7 bottom-8">{props.descr}</h2>
         </div>
     )
 }
