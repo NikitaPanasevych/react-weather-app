56b85d9f666b549852a748043a6a7d04d7e7ee08
DeStRaaM <20destraam03@gmail.com>
2022-08-05 22:34:07 +0200

    Added delete function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Card from './Card';
 import { useEffect, useState } from 'react';
+import { Button } from '@mui/material';
+import TextField from '@mui/material/TextField';
 
 const App = () => {
 
@@ -10,15 +12,12 @@ const App = () => {
 
   var locationAPI = ["https://api.openweathermap.org/data/2.5/weather?q=" + searchedLocation +"&appid=489ec2c665aaa26c4f89caee27f9a6f6&units=metric"]
 
-  const createCard = (cityArray) =>{
-    return(
-      <Card
-        id={cityArray.id}
-        city = {cityArray.city} 
-        temp = {Math.round(cityArray.temp)}
-        descr = {cityArray.descr}
-        img = {"http://openweathermap.org/img/wn/" + cityArray.img + "@2x.png"}
-       />)
+  const handleDelete = (id) =>{
+    setCityArray(prevNotes => {
+      return prevNotes.filter((Card, index) => {
+        return index !== id;
+      });
+    });
   }
 
   const handleInput=(event)=>{
@@ -36,16 +35,16 @@ const App = () => {
             .then((data) =>{
             return(
               setCityArray(prevValue => {return(
-                (cityArray.id === data.weather[0].id) ? [...prevValue]
-                :
+                data.name !== undefined ? 
                 [...prevValue,
                  {city:data.name,
                   temp:data.main.temp,
                   descr:data.weather[0].description,
                   img:data.weather[0].icon,
-                  id:data.weather[0].id
                 }
                 ]
+                :
+                [...prevValue]
                 )})  
               )})
               :
@@ -53,14 +52,30 @@ const App = () => {
   },[searchedLocation])
 
   return (
-    <div className="App w-screen">
+    <div className="App w-screen min-h-screen bg-gradient-to-tr from-[#B983FF] to-[#99FEFF]">
       <div className='text-center'>
         <h1 className='text-6xl pb-10 pt-10'>Weather API App</h1>
-        <input onChange={handleInput} placeholder="Type a city" className='text-2xl h-10 w-1/4 rounded-lg focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500' />
-        <p><button onClick={handleClick} type='submit' className='mt-4 mb-10 h-14 w-1/12 ease-in-out duration-300 hover:bg-[#D61C4E]  bg-[#7D9D9C] rounded-full'>Add city</button></p>
+        <TextField id="outlined-basic" className=' w-96' onChange={handleInput} label="Type a city" variant="outlined" />
+        <p className='m-8'>
+          <Button onClick={handleClick} className=" w-36" variant="contained">Add city</Button>
+        </p>
       </div>
-      <div className='flex flex-wrap m-auto w-3/4 mt-10'>
-      {cityArray.map(createCard)}
+      <div className='flex flex-wrap m-auto w-3/4'>
+      {cityArray.map((cityArray, index) =>{
+              return(
+                <Card
+                  id={index}
+                  key={index}
+                  city = {cityArray.city} 
+                  temp = {Math.round(cityArray.temp)}
+                  descr = {cityArray.descr}
+                  img = {"http://openweathermap.org/img/wn/" + cityArray.img + "@2x.png"}
+                  onDelete = {handleDelete}
+                />
+              )
+            }
+          )
+        }
       </div>
     </div>
   );
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,21 @@
+import Button from "@mui/material/Button";
+import DeleteIcon from '@mui/icons-material/Delete';
+
 const Card = (props) => {
+
+    const handleClick = () => {
+        props.onDelete(props.id)
+    }
     
     return(
-        <div className="w-96 h-96 m-auto text-center pt-8 text-4xl rounded-3xl border-4 bg-[#A2B5BB] justify-center">
+        <div className="w-96 h-96 m-auto text-center pt-8 mb-20 text-4xl rounded-3xl bg-[#94B3FD] border-4 border-black">
             <h1 className="">{props.city}</h1>
             <h2 className="relative font-semibold top-3">{props.temp}°C</h2>
             <img className="h-48 w-48 m-auto relative bottom-4" src={props.img} />
             <h2 className="relative w-3/4 m-auto bottom-8">{props.descr}</h2>
+            <Button onClick={handleClick} variant="outlined" color="error" className="bottom-6" startIcon={<DeleteIcon />}>
+                Delete
+            </Button>
         </div>
     )
 }
